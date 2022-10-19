5fa19233ad447e88639b14b78bd5c41d9bc9f150
Mohammad Sarsour <SarsourM@gmail.com>
2022-10-19 19:21:44 -0400

    Draft of Personal Budget Tracker Activity

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+// console.log(weeklyExpenses,monthlyExpenses,annualExpenses)
+console.log(weeklyExpenseQuestions)
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + response
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + response
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + response
+}
